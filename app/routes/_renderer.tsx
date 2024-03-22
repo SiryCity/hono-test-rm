d6bcd0ed86d42725754730804103404918d36fdb
Mizutani <sirycity2018@gmail.com>
2024-03-22 17:51:29 +0900

    tailwind導入

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,14 +1,18 @@
 import { Style } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
 import { Script } from 'honox/server'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import styles from '../style.css?url'
 
 export default jsxRenderer(({ children, title }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
+        <link href={styles} rel="stylesheet" />
         <Script src="/app/client.ts" async />
         <Style />
       </head>
