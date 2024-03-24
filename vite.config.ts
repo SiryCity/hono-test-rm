1b903f325a2b07d2fedd7a199e3e729df1652a75
Mizutani <sirycity2018@gmail.com>
2024-03-25 01:11:20 +0900

    processenvがいる？

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,17 @@ export default defineConfig(({ mode }) => {
     return {
       plugins: [client()],
       resolve: { alias: { '~/': __dirname + '/app/' } },
+      define: {
+        FOO: process.env.FOO,
+      },
     }
   } else {
     return {
       plugins: [honox(), pages()],
       resolve: { alias: { '~/': __dirname + '/app/' } },
+      define: {
+        FOO: process.env.FOO,
+      },
       build: {
         assetsDir: 'static',
         ssrEmitAssets: true,
