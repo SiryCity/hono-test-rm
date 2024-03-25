f4fb5cf486b20b9fa2c5693f4aaf8ba87d995b0a
Mizutani <sirycity2018@gmail.com>
2024-03-25 13:50:36 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,23 +7,14 @@ import { ViteToml } from 'vite-plugin-toml'
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
     return {
-      define: {
-        FOO: `"${process.env.FOO}"`,
-      },
       plugins: [client()],
       resolve: { alias: { '~/': __dirname + '/app/' } },
     }
   } else {
     return {
-      define: {
-        FOO: `"${process.env.FOO}"`,
-      },
       plugins: [ViteToml(), honox(), pages()],
       resolve: { alias: { '~/': __dirname + '/app/' } },
-      build: {
-        assetsDir: 'static',
-        ssrEmitAssets: true,
-      },
+      build: { assetsDir: 'static', ssrEmitAssets: true },
     }
   }
 })
