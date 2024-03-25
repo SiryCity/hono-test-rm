ff031d6af752a557a62a0d63ed1fa60d4f14074a
Mizutani <sirycity2018@gmail.com>
2024-03-25 13:27:35 +0900

    環境変数もういける？

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
 import pages from '@hono/vite-cloudflare-pages'
+import dotenv from 'dotenv'
 import honox from 'honox/vite'
 import client from 'honox/vite/client'
 import { defineConfig } from 'vite'
 import { ViteToml } from 'vite-plugin-toml'
 
+dotenv.config() // load env vars from .env
+
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
     return {
-      plugins: [ViteToml(), client()],
+      define: {
+        FOO: `"${process.env.FOO}"`,
+      },
+      plugins: [
+        // ViteToml(),
+        client(),
+      ],
       resolve: { alias: { '~/': __dirname + '/app/' } },
     }
   } else {
     return {
-      plugins: [ViteToml(), honox(), pages()],
+      define: {
+        FOO: `"${process.env.FOO}"`,
+      },
+      plugins: [
+        // ViteToml(),
+        honox(),
+        pages(),
+      ],
       resolve: { alias: { '~/': __dirname + '/app/' } },
       build: {
         assetsDir: 'static',
