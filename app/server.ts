1fb8ece3f27ef66830f858cb1810a3a4f30f42f5
Mizutani <sirycity2018@gmail.com>
2024-03-27 16:55:28 +0900

    favicon検証

--- app/server.ts
+++ app/server.ts
@@ -1,10 +1,7 @@
-import { serveStatic } from '@hono/node-server/serve-static'
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
 const app = createApp()
-
-app.use('public/*', serveStatic({ root: './dist' }))
 
 showRoutes(app)
 
