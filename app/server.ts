7edaffd8fa738b793592d46642acd826f1d6228d
Mizutani <sirycity2018@gmail.com>
2024-03-27 16:48:38 +0900

    test

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,10 @@
+import { serveStatic } from '@hono/node-server/serve-static'
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
 const app = createApp()
+
+app.use('public/*', serveStatic({ root: './dist' }))
 
 showRoutes(app)
 
