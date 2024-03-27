feaae325bf6308b535244c003597b68de84021e4
Mizutani <sirycity2018@gmail.com>
2024-03-27 17:11:15 +0900

    いけるか

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,10 @@
+import { serveStatic } from '@hono/node-server/serve-static'
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
 const app = createApp()
+
+app.use('/opengraph-image.jpg', serveStatic())
 
 showRoutes(app)
 
